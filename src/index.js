a6729f8d53c26e34c08f034edd757f1ba3435bdb
kagannikita <kagan.nikita@bk.ru>
2021-03-03 11:49:31 +0600

    feat: add function min, max, arr

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+    if ( array != null && array.length != null && array.length > 0){
+        return Math.min(...array)
+    }
+        return 0
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ( array != null && array.length != null && array.length > 0){
+        return Math.max(...array)
+    }
+    return 0
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ( array != null && array.length != null && array.length > 0) {
+        const numbers = (accumulator, currentValue) => accumulator + currentValue;
+        return array.reduce(numbers) / array.length;
+    }
+    return 0
 }
